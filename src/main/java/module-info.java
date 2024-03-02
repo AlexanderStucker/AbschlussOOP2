122647295952dcbbb15c27cb71eeef4e207cf66d
Alexander Stucker <alexander.stucker@hftm.ch>
2024-03-02 13:07:05 +0100

    Added TableView and Rows for a cleaner UI

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,6 +3,7 @@ module movies.rate {
     requires javafx.controls;
     requires javafx.fxml;
 
+    opens movies.rate.model to javafx.base;
     opens movies.rate.controller to javafx.fxml;
     exports movies.rate;
     exports movies.rate.controller;
